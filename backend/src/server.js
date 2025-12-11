fa40ecabba357314330cef5aac1c354e25af1edc
S-nusrath <nusrath4786@gmail.com>
2025-12-11 18:58:36 +0530

    Updated backend code

--- backend/src/server.js
+++ backend/src/server.js
@@ -8,7 +8,13 @@ import "./models/user.model.js";
 dotenv.config();
 
 const app = express();
-app.use(cors());
+//app.use(cors());
+//const cors = require("cors");
+app.use(cors({
+    origin: "http://localhost:5173",   // VITE runs on 5173
+    credentials: true
+}));
+
 app.use(express.json());
 
 // Routes
