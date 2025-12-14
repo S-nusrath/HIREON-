bcae244e329aea10d80edd97151ac5ede2782973
S-nusrath <nusrath4786@gmail.com>
2025-12-14 17:34:55 +0530

    Fix socket-io and job realrtime updates

--- backend/src/socket.js
+++ backend/src/socket.js
@@ -1,3 +1,34 @@
+// import { Server } from "socket.io";
+
+// let io;
+
+// export const initSocket = (server) => {
+//   io = new Server(server, {
+//     cors: {
+//       origin: "http://localhost:5173",
+//       methods: ["GET", "POST"],
+//       credentials: true,
+//     },
+//   });
+
+//   io.on("connection", (socket) => {
+//     console.log("🔥 New client connected:", socket.id);
+
+//     socket.on("disconnect", () => {
+//       console.log("❌ Client disconnected:", socket.id);
+//     });
+//   });
+
+//   return io;
+// };
+
+// export const getIO = () => {
+//   if (!io) throw new Error("Socket.io not initialized");
+//   return io;
+// };
+
+
+
 import { Server } from "socket.io";
 
 let io;
@@ -23,7 +54,8 @@ export const initSocket = (server) => {
 };
 
 export const getIO = () => {
-  if (!io) throw new Error("Socket.io not initialized");
+  if (!io) {
+    throw new Error("Socket.io not initialized");
+  }
   return io;
 };
-
